d9f24b32243d74a6fe8aa5bd486a3801a0c6be3c
Aryan Kulkarni <aryangkulkarni@gmail.com>
2024-02-01 19:09:25 +0530

    Table modal added

--- src/components/Bookmark.js
+++ src/components/Bookmark.js
@@ -9,6 +9,20 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  backgroundColor: '#415A77',
+  position: 'fixed',
+  top: '30%',
+  left: '50%',
+  transform: 'translateX(-50%)',
+  width: '100%',  // Adjust the width as needed
+  maxHeight: '70%',  // Adjust the height as needed
+  overflowY: 'auto',
+  borderTopLeftRadius: 30,
+  // borderTopRightRadius: 30,
+};
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
@@ -31,6 +45,11 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 const Bookmark = () => {
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const Initial = []
   const [problems, setProblems] = useState(Initial);
 
@@ -75,36 +94,77 @@ const Bookmark = () => {
   }, [])
 
   return (
-    <TableContainer component={Paper} style={{ backgroundColor: '#415A77', width: '45vw', borderRadius: 20, maxHeight: 300, overflowY: 'hidden'}}>
-      <Table sx={{ width: '45vw' }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Index</StyledTableCell>
-            <StyledTableCell align="left">Bookmarks</StyledTableCell>
-            <StyledTableCell align="right"></StyledTableCell>
-            <StyledTableCell align="right"></StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {problems.map((problem, index) => (
-            <StyledTableRow key={problem.index}>
-              <StyledTableCell component="th" scope="row">
-                {problem.index}
-              </StyledTableCell>
-              <StyledTableCell align="left">{problem.name}</StyledTableCell>
-              <StyledTableCell align="left">
-                <Link onClick={() => deleteProblem(problem._id)} className="text-sm text-indigo-400 font-semibold hover:underline hover:text-indigo-500 cursor-pointer"><Button style={{ backgroundColor: '#0D1B2A' }} variant="contained">Remove</Button></Link>
-              </StyledTableCell>
-              <StyledTableCell align="left">
-                <a href={`https://codeforces.com/problemset/problem/${problem.contestId}/${problem.index}`} target='_blank' className="text-sm text-indigo-400 font-semibold hover:underline hover:text-indigo-500" rel="noreferrer"><Button style={{ backgroundColor: '#0D1B2A' }} variant="contained">Solve</Button></a>
-              </StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div>
+
+      <TableContainer component={Paper} style={{ backgroundColor: '#415A77', width: '45vw', borderRadius: 20, maxHeight: 300, overflowY: 'hidden' }} onClick={handleOpen}>
+        <Table sx={{ width: '45vw' }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Index</StyledTableCell>
+              <StyledTableCell align="left">Bookmarks</StyledTableCell>
+              <StyledTableCell align="right"></StyledTableCell>
+              <StyledTableCell align="right"></StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {problems.map((problem, index) => (
+              <StyledTableRow key={problem.index}>
+                <StyledTableCell component="th" scope="row">
+                  {problem.index}
+                </StyledTableCell>
+                <StyledTableCell align="left">{problem.name}</StyledTableCell>
+                <StyledTableCell align="left">
+                  <Link onClick={() => deleteProblem(problem._id)} className="text-sm text-indigo-400 font-semibold hover:underline hover:text-indigo-500 cursor-pointer"><Button style={{ backgroundColor: '#0D1B2A' }} variant="contained">Remove</Button></Link>
+                </StyledTableCell>
+                <StyledTableCell align="left">
+                  <a href={`https://codeforces.com/problemset/problem/${problem.contestId}/${problem.index}`} target='_blank' className="text-sm text-indigo-400 font-semibold hover:underline hover:text-indigo-500" rel="noreferrer"><Button style={{ backgroundColor: '#0D1B2A' }} variant="contained">Solve</Button></a>
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <TableContainer component={Paper} sx={style}>
+          <Table sx={{ width: '100%' }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Index</StyledTableCell>
+                <StyledTableCell align="left">Bookmarks</StyledTableCell>
+                <StyledTableCell align="right"></StyledTableCell>
+                <StyledTableCell align="right"></StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {problems.map((problem, index) => (
+                <StyledTableRow key={problem.index}>
+                  <StyledTableCell component="th" scope="row">
+                    {problem.index}
+                  </StyledTableCell>
+                  <StyledTableCell align="left">{problem.name}</StyledTableCell>
+                  <StyledTableCell align="left">
+                    <Link onClick={() => deleteProblem(problem._id)} className="text-sm text-indigo-400 font-semibold hover:underline hover:text-indigo-500 cursor-pointer"><Button style={{ backgroundColor: '#0D1B2A' }} variant="contained">Remove</Button></Link>
+                  </StyledTableCell>
+                  <StyledTableCell align="left">
+                    <a href={`https://codeforces.com/problemset/problem/${problem.contestId}/${problem.index}`} target='_blank' className="text-sm text-indigo-400 font-semibold hover:underline hover:text-indigo-500" rel="noreferrer"><Button style={{ backgroundColor: '#0D1B2A' }} variant="contained">Solve</Button></a>
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Modal>
+    </div>
   );
 
 }
+
+
 
 export default Bookmark
--- src/components/Unsolved.js
+++ src/components/Unsolved.js
@@ -11,6 +11,21 @@ import { styled } from '@mui/material/styles';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useGlobalContext } from '../context/Context';
+import Modal from '@mui/material/Modal';
+
+
+const style = {
+    backgroundColor: '#415A77',
+    position: 'fixed',
+    top: '30%',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    width: '100%',  // Adjust the width as needed
+    maxHeight: '70%',  // Adjust the height as needed
+    overflowY: 'auto',
+    borderTopLeftRadius: 30,
+    // borderTopRightRadius: 30,
+};
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
     '&:nth-of-type(odd)': {
@@ -33,6 +48,12 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 const Unsolved = (props) => {
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+
     const { globalVariable } = useGlobalContext();
     const [problems, setProblems] = useState([]);
     useEffect(() => {
@@ -65,33 +86,72 @@ const Unsolved = (props) => {
 
 
     return (
-        <TableContainer component={Paper} style={{ backgroundColor: '#415A77', width: '45vw', height:300, borderRadius: 20,overflowY: 'hidden' }}>
-            <Table sx={{ width: '45vw' }} aria-label="customized table">
-                <TableHead>
-                    <TableRow>
-                        <StyledTableCell>Index</StyledTableCell>
-                        <StyledTableCell align="left">Unsolved Problems</StyledTableCell>
-                        <StyledTableCell align="right"></StyledTableCell>
-                        <StyledTableCell align="right"></StyledTableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {problems.map((problem, index) => (
-                        <StyledTableRow key={problem.name}>
-                            <StyledTableCell component="th" scope="row">
-                                {problem.index}
-                            </StyledTableCell>
-                            <StyledTableCell align="left">{problem.name}</StyledTableCell>
-                            <StyledTableCell align="left">
-                            </StyledTableCell>
-                            <StyledTableCell align="left">
-                                <a href={`https://codeforces.com/problemset/problem/${problem.contestId}/${problem.index}`} target='_blank' className="text-sm text-indigo-400 font-semibold hover:underline hover:text-indigo-500" rel="noreferrer"><Button style={{ backgroundColor: '#0D1B2A' }} variant="contained">Solve</Button></a>
-                            </StyledTableCell>
-                        </StyledTableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <div>
+
+            <TableContainer component={Paper} style={{ backgroundColor: '#415A77', width: '45vw', height: 300, borderRadius: 20, overflowY: 'hidden' }} onClick={handleOpen}>
+                <Table sx={{ width: '45vw' }} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>Index</StyledTableCell>
+                            <StyledTableCell align="left">Unsolved Problems</StyledTableCell>
+                            <StyledTableCell align="right"></StyledTableCell>
+                            <StyledTableCell align="right"></StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {problems.map((problem, index) => (
+                            <StyledTableRow key={problem.name}>
+                                <StyledTableCell component="th" scope="row">
+                                    {problem.index}
+                                </StyledTableCell>
+                                <StyledTableCell align="left">{problem.name}</StyledTableCell>
+                                <StyledTableCell align="left">
+                                </StyledTableCell>
+                                <StyledTableCell align="left">
+                                    <a href={`https://codeforces.com/problemset/problem/${problem.contestId}/${problem.index}`} target='_blank' className="text-sm text-indigo-400 font-semibold hover:underline hover:text-indigo-500" rel="noreferrer"><Button style={{ backgroundColor: '#0D1B2A' }} variant="contained">Solve</Button></a>
+                                </StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <TableContainer component={Paper} sx={style}>
+                    <Table sx={{ width: '100%' }} aria-label="customized table">
+                        <TableHead>
+                            <TableRow>
+                                <StyledTableCell>Index</StyledTableCell>
+                                <StyledTableCell align="left">Unsolved Problems</StyledTableCell>
+                                <StyledTableCell align="right"></StyledTableCell>
+                                <StyledTableCell align="right"></StyledTableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {problems.map((problem, index) => (
+                                <StyledTableRow key={problem.name}>
+                                    <StyledTableCell component="th" scope="row">
+                                        {problem.index}
+                                    </StyledTableCell>
+                                    <StyledTableCell align="left">{problem.name}</StyledTableCell>
+                                    <StyledTableCell align="left">
+                                    </StyledTableCell>
+                                    <StyledTableCell align="left">
+                                        <a href={`https://codeforces.com/problemset/problem/${problem.contestId}/${problem.index}`} target='_blank' className="text-sm text-indigo-400 font-semibold hover:underline hover:text-indigo-500" rel="noreferrer"><Button style={{ backgroundColor: '#0D1B2A' }} variant="contained">Solve</Button></a>
+                                    </StyledTableCell>
+                                </StyledTableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+
+            </Modal>
+        </div>
     )
 }
 
